abfcbd131d138a8c171616e28954f92a878face7
Sean Clifford <sclifford6262@gmail.com>
2018-05-09 03:13:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blocks){
   return function(street1, street2){
     route = Math.abs(parseInt(street1) - parseInt(street2))
     if(route > blocks){
-      `${route - blocks} out of range`
+      `${route - blocks} blocks out of range`
     } else {
       `within range by ${blocks - route}`
     }
